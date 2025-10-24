2ff775da484035184fb700bb9cac525960270d2c
Farmohammed07 <farooqmohd5744@gmail.com>
2025-10-24 20:43:37 +0530

    Color

--- src/App.js
+++ src/App.js
@@ -21,11 +21,12 @@ function App() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto justify-content-end w-100'>
-              <Nav.Link href='/' className='active text-uppercase'>Home</Nav.Link>
-              <Nav.Link href='/menu' className='text-uppercase'>Menu</Nav.Link>
-              <Nav.Link href='/about' className='text-uppercase'>About</Nav.Link>
-              <Nav.Link href='/contact' className='text-uppercase'>Contact</Nav.Link>
-            </Nav>
+  <Nav.Link href='/' className='hover-blue active text-uppercase'>Home</Nav.Link>
+  <Nav.Link href='/menu' className='hover-blue text-uppercase'>Menu</Nav.Link>
+  <Nav.Link href='/about' className='hover-blue text-uppercase'>About</Nav.Link>
+  <Nav.Link href='/contact' className='hover-blue text-uppercase'>Contact</Nav.Link>
+</Nav>
+
           </Navbar.Collapse>
         </Container>
       </Navbar>
